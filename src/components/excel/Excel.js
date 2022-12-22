8514b076fa92bd85765f587a952bf6227f74c2c8
gnulitskaya <webgnule@mail.ru>
2022-12-22 17:14:47 +0300

    add html

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -6,10 +6,15 @@ export class Excel {
 
   getRoot() {
     const $root = document.createElement('div')
+    $root.classList.add('excel')
 
     this.components.forEach(Component => {
-      const component = new Component()
-      $root.insertAdjacentHTML('beforeend', component.toHTML())
+      const $el = document.createElement('div')
+      $el.classList.add(Component.className)
+      const component = new Component($el)
+      $el.innerHTML = component.toHTML()
+      $root.append($el)
+      // $root.insertAdjacentHTML('beforeend', component.toHTML())
     });
     
     return $root
