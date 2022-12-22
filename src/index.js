77426495be95d3467d858f928e88f00535616d74
gnulitskaya <webgnule@mail.ru>
2022-12-22 15:30:49 +0300

    add render method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
-import { Excel } from './components/excel/Excel'
+import {Excel} from '@/components/excel/Excel'
+import {Header} from '@/components/header/Header'
+import {Toolbar} from '@/components/toolbar/Toolbar'
+import {Formula} from '@/components/formula/Formula'
+import {Table} from '@/components/table/Table'
 import './scss/index.scss'
 
 const excel = new Excel(
     '#app', {
-        componets: []
+        components: [Header, Toolbar, Formula, Table]
     }
 )
-console.log(excel);
+excel.render();
