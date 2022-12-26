f7015ae66ea098af00407e76ace446aacf2139ef
gnulitskaya <v.gnulitskaya@finflowsoft.com>
2022-12-26 15:19:24 +0300

    edit table.template

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,9 +3,9 @@ const CODES = {
     Z: 90
 }
 
-function createCell() {
+function toCell() {
     return `
-    <div class="cell" contenteditable="">B2</div>
+    <div class="cell" contenteditable=""></div>
     `
 }
 
@@ -13,10 +13,10 @@ function toColumn(col) {
     return `<div class="column">${col}</div>`
 }
  
-function createRow(content) {
+function createRow(index, content) {
     return `
     <div class="row">
-        <div class="row-info"></div>
+        <div class="row-info">${index ? index : ''}</div>
         <div class="row-data">${content}</div>
     </div>
     `
@@ -36,12 +36,15 @@ export function createTable(rowsCount = 15) {
         .map(toColumn) // el => createCol(el)
         .join('')
 
-    console.log(cols);
-
-    rows.push(createRow(cols))
-
+    rows.push(createRow(null, cols))
+  
     for (let i = 0; i < rowsCount; i++) {
-        rows.push(createRow())
+        const cells = new Array(colsCount)
+        .fill('')
+        .map(toCell)
+        .join('')
+
+        rows.push(createRow(i+1, cells))
     }
 
     return rows.join('')
